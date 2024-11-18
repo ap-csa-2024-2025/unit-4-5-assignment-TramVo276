2be1382d4f06ee936277bd9d928be483adf09f33
TramVo276 <tvo941352@gmail.com>
2024-11-18 10:17:20 -0800

    sdgad

--- Main.java
+++ Main.java
@@ -4,17 +4,33 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    for (int i = 0; i < word.length(); i++)
+    {
+      String L = word.substring(i, i+1);
+      // REPEAT N TIMES
+      for (int j = 0; j < N; j++)
+      {
+        System.out.println(L);
+      }
+    }
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (i = 10; i > 0; i--)
+    {
+      for j = 0; j < i; j++
+      {
+        System.out.println(i);
+      }
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Input a number: ");
+    for (i = 0)
   }
 
   public static void uprightNumberTriangle(int N)
